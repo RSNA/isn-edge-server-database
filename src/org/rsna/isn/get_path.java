140fdcdd63fc64064eb90ee1285d58fdb814be99
Steve Langer / Mayo <sglanger@nibib-2.wusmpriv.wustl.edu>
2010-11-02 13:26:37 -0500

    log_cat with get_path to remove any OS dependency on RSNA paths

---
+++ src/org/rsna/isn/get_path.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.rsna.isn ;
+
+/**
+ * @author
+ * @version
+ * @param
+ * @return
+ * Package name: org.rsna.isn.xxx
+ * Purpose: Provide an OS neutral way to inform the caller the paths to
+ *          RSNA folders
+ * External Dependencies:
+ *
+ * Copyright (c) <2010>, <Radiological Society of North America>
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the <RSNA> nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+public class get_path {
+
+    public String path_root ;
+    public String path_logs ;
+    public String path_properties ;
+    public String path_dcm_in ;
+    public String path_dcm_out ;
+    
+    /**
+     *
+     * @return  Constructors are private and cannot have returns
+     * Purpose: determine the OS, and then stuff paths for caller
+     */
+    get_path () {
+        String os = System.getProperty("os.name").toLowerCase() ;
+        //System.out.println ("os =" + os);
+        
+        if (os.contains("wind")) {
+            path_root = "c:\rsna";
+            //path_logs = "c:\rsna\logs";
+            //path_properties = "c:\rsna\properties";
+            //path_dcm_in = "c:\rsna\dcm";
+            //path_dcm_out = "c:\rsna\dcmout";
+
+        } else if (os.contains("lin")) {
+            path_root = "/rsna/";
+            path_logs = "/rsna/logs/";
+            path_properties = "/rsna/properties/";
+            path_dcm_in = "/rsna/dcm/";
+            path_dcm_out = "/rsna/dcmout/";
+        }
+    }
+}
