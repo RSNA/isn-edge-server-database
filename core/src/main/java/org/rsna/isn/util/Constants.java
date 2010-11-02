75cd72c656e0bd3983bfa5a3dfd83eb3ce4a114a
Wyatt Tellis (UCSF) <wtellis@nibib-2.wusmpriv.wustl.edu>
2010-11-02 09:39:45 -0500

    Changed assigning authority namespace and OID to unique values to prevent disruption to UoC testing.

--- core/src/main/java/org/rsna/isn/util/Constants.java
+++ core/src/main/java/org/rsna/isn/util/Constants.java
@@ -10,9 +10,9 @@ package org.rsna.isn.util;
  */
 public class Constants
 {
-	public static final String NAMESPACE_ID = "IHENA";
+	public static final String NAMESPACE_ID = "RSNA-ISN-TEST";
 	
-	public static final String UNIVERSAL_ID = "1.3.6.1.4.1.21367.2009.1.2.300";
+	public static final String UNIVERSAL_ID = "2.25.254116628432602510754375974119947061420";
 
 	public static final String UNIVERSAL_ID_TYPE = "ISO";
 
