23e1eee7bc686afc63022f5e6b80ca3d6ca1acf1
Wayne LaBelle <wlabelle@umm.edu>
2010-11-03 09:15:04 -0400

    Revert "final log_cat"
    This reverts commit 8f1af80ae7c7485d6a325e19ff270c62bb705fcc.

---
+++ RSNADB rollout v1.1.1.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE hipaa_audit_views
+(
+  id serial NOT NULL,
+  requesting_ip character varying(15),
+  requesting_username character varying(100),
+  requesting_uri text,
+  modified_date timestamp with time zone
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE hipaa_audit_mrns
+(
+  id serial NOT NULL,
+  view_id integer,
+  mrn character varying(100),
+  modified_date timestamp with time zone
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE hipaa_audit_accession_numbers (
+  id serial NOT NULL,
+  view_id integer,
+  accession_number character varying(100),
+  modified_date timestamp with time zone
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE patient_rsna_ids
+ ALTER COLUMN rsna_id TYPE character varying(30);
---
+++ RSNADB rollout v1.1.2.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE patient_rsna_ids ADD COLUMN patient_alias_lastname character varying(100);
+ALTER TABLE patient_rsna_ids ADD COLUMN patient_alias_firstname character varying(100);
