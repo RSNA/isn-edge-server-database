d3da4942c9354c6ffe1a10af66262586e30890d8
Wendy Zhu <wzhu@radiology.bsd.uchicago.edu>
2011-07-27 13:07:18 -0500

    /*** version2.0.2 Add column to mark user active or not. ***/ 1.	Add column “active” into table “user”. By default, all account are true. 2.	Add table “schema_version” store the database schema version.

---
+++ RSNADB rollout v2.0.2.sql
@@ -0,0 +1,17 @@
+﻿-- activate or deactivate a user account
+ALTER TABLE users ADD COLUMN active Boolean DEFAULT true;
+UPDATE users SET active=true;
+
+-- store database schema version
+CREATE TABLE schema_version
+(
+  id serial NOT NULL,
+  version character varying,
+  modified_date timestamp with time zone DEFAULT now(),
+  CONSTRAIN pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE schema_version OWNER TO edge;
+COMMENT ON TABLE schema_version IS 'Store database schema version';
