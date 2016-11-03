435bfddfbc57e164e732a9dfadb36b2a55d7be06
wendyzhu <wzhu@radiology.bsd.uchicago.edu>
2016-11-03 12:07:08 -0500

    DB update for v5.0
    Per Clifton's request, update values in email_configurations, sms_configurations, status_codes

--- RSNADB.rollout.v5.0.0.sql
+++ RSNADB.rollout.v5.0.0.sql
@@ -53,7 +53,7 @@ INSERT INTO sms_configurations VALUES('enable_sms','false',now());
 INSERT INTO sms_configurations VALUES('account_id','',now());
 INSERT INTO sms_configurations VALUES('token','',now());
 INSERT INTO sms_configurations VALUES('sender','',now());
-INSERT INTO sms_configurations VALUES('body', 'Your RSNA Image Share Access Code is $accesscode$.',now());
+INSERT INTO sms_configurations VALUES('body','Your imaging results are ready to be accessed. Your Access Code is $accesscode$. Instructions available at http://www.rsna.org/image_share.aspx.',now());
 
 -- Table: sms_jobs
 CREATE TABLE sms_jobs
@@ -73,3 +73,12 @@ WITH (
 );
 ALTER TABLE sms_jobs OWNER TO edge;
 COMMENT ON TABLE sms_jobs IS 'This table is used to store queued SMS messages. Jobs within the queue will be handled by a worker thread which is responsible for handling any send failures and retrying failed jobs';
+
+--Update email content
+UPDATE email_configurations 
+SET value = '<b><font size="24">RSNA Image Share</font></b><br><b><i><font size="5">Take Control of Your Medical Images</font></i></b></h2><br><br><br>Dear $patientname$,<br><br>Welcome to Image Share, a network designed to enable patients to access and control their medical imaging results. Image Share was developed by the Radiological Society of North America (RSNA) and its partners, with funding from the National Institute of Biomedical Imaging and Bioengineering.<br><br>You are receiving this message because the radiology staff at $site_id$ have sent your imaging results to a secure online data repository so you can access them.<br><br>To access your images:<br><br><ol><li><b>Create a personal health record (PHR) account on one of the participating image-enabled PHR systems.</b> If you created an account following an earlier visit, simply log in. You can create an account on one of the following participating sites:<br><br><ul><li>DICOM Grid:<a href="http://imageshare.dicomgrid.com">http://imageshare.dicomgrid.com</a></li><li>lifeIMAGE: <a href="https://cloud.lifeimage.com/rsna/phr">https://cloud.lifeimage.com/rsna/phr</a></li></ul><br><br>Each of these sites provides detailed instructions on creating an account and using it to retrieve your imaging results. Be careful to record your PHR account log in information (PHR provider, user name and password) and keep it secure, as you do with all your valuable online information.</li><br><br><li><b>Use your PHR account to access and take control of your imaging results.</b> Once you’ve created an account, you’ll just need to log in and provide two pieces of information to access your images and reports:<br><br>Your Access Code: <b>$accesscode$</b><br>Your date of birth<br><br>That’s all you need to retrieve the images and report into your PHR account. You can then use your PHR account to share information with others you trust, including care providers. They can view the images and the report anywhere Internet access is available. Some PHRs enable you to email a link to your images, so a provider can view your examinations without you needing to be present.</li></ol><br><br>User support is available during business hours at <a href="mailto:helpdesk@imsharing.org">helpdesk@imsharing.org</a> | Toll-free: 1-855-IM-SHARING (467-4274).' 
+, modified_date = now()
+WHERE key = patient_email_body;
+
+--Add status code
+INSERT INTO status_codes (status_code,description) VALUES (-1,'No devices found');
